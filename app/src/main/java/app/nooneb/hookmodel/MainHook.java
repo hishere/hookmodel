71166d4996ff7847d25a22f50fccd7656439bd35
Nooneb <1971902975@qq.com>
2024-10-13 15:22:16 +0800

    第一次提交

---
+++ app/src/main/java/app/nooneb/hookmodel/MainHook.java
@@ -0,0 +1,14 @@
+package app.nooneb.hookmodel;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class MainHook implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+
+        if (lpparam.packageName.contains("com.eg")){
+
+        }
+    }
+}
